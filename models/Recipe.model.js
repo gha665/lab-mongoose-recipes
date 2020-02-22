c934b18c15ed4f383b624b6a06f40082cba7716e
Evgeny Galitskiy <gha665@gmail.com>
2020-02-21 23:59:16 -0500

    first commit

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: { type: String, required: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef '] },
+  ingredients: { type: Array },
+  cuisune: { type: String, required: true },
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
